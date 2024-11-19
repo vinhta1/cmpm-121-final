491c176f0180d9a1f267889ec0c59e06ddc13c6c
lmurayam <130516683+lmurayam@users.noreply.github.com>
2024-11-19 00:46:44 +0000

    Adding p5js

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,22 @@
+import p5 from 'p5';
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
-const test : HTMLHeadingElement = document.createElement("h1");
+const test: HTMLHeadingElement = document.createElement("h1");
 test.innerHTML = "TEST"
 app.appendChild(test);
+
+
+
+const sketch = (p) => {
+    p.setup = () => {
+        p.createCanvas(400, 400);
+    };
+
+    p.draw = () => {
+        p.background(220);
+        p.fill(255, 0, 0);
+        p.ellipse(200, 200, 100, 100);
+    };
+};
+
+new p5(sketch);
