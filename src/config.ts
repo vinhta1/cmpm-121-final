386e1ec6de970635f664e1d1869fbf72f5791136
lmurayam <130516683+lmurayam@users.noreply.github.com>
2024-11-23 07:32:46 +0000

    use vite static asset import

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,7 @@
+// @ts-ignore: <Vite uses import on static assets to get the resolved public URL, I'm too tired to figure out how to make deno happy about that>
+
+import test from "./assets/test_pixel.png";
+
 export const IMAGE_PATHS: string[] = [
-  "src/assets/test_pixel.png",
+  `${test}`,
 ];
