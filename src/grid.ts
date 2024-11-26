11d1fe3890ef4ec8495d9ffd201cba0778247c67
Vinh Ta <tavinh132@gmail.com>
2024-11-25 17:56:51 -0800

    added getters and setters to grid properties

--- src/grid.ts
+++ src/grid.ts
@@ -11,76 +11,93 @@ export interface GridCell {
 const OFFSET = 7;
 
 export class Grid {
-  private buffer: ArrayBuffer;
-  private viewer: Uint8Array;
-  private numCells: number;
-  private width: number;
+  private _numCells: number;            public get numCells(): number { return this._numCells; }            public set numCells(value: number) { this._numCells = value; }
+  private _buffer: ArrayBuffer;         public get buffer(): ArrayBuffer { return this._buffer; }           public set buffer(value: ArrayBuffer) { this._buffer = value;}
+  private _viewer: Uint8Array;          public get viewer(): Uint8Array { return this._viewer; }            public set viewer(value: Uint8Array) { this._viewer = value; }
+  //stores all the viewers in an array 
+  private _stateArray: Uint8Array[];    public get stateArray(): Uint8Array[] { return this._stateArray; }  public set stateArray(value: Uint8Array[]) { this._stateArray = value; }
+  private _width: number;               public get width(): number { return this._width; }                  public set width(value: number) { this._width = value; }
 
   constructor(width: number, height: number) {
     // Create an array buffer to store all cells. Cells are comprised of 7 uInt8, depicted in Interface GridCell
-    this.numCells = width * height;
-    this.buffer = new ArrayBuffer(this.numCells * OFFSET);
-    this.viewer = new Uint8Array(this.buffer);
-    this.width = width;
+    this._numCells = width * height;
+    this._buffer = new ArrayBuffer(this._numCells * OFFSET);
+    this._viewer = new Uint8Array(this._buffer); //game state - everything except player location. will need array of this
+    this._stateArray = [];
+    this._width = width;
 
     let x = 0;
     let y = 0;
-    for (let i = 0; i < this.numCells; i++) {
-      this.viewer[i * OFFSET] = x;
-      this.viewer[i * OFFSET + 1] = y;
-      x == this.width - 1 ? (x = 0, y++) : x++;
-      this.viewer[i * OFFSET + 2] = 0;
-      this.viewer[i * OFFSET + 3] = 0;
-      this.viewer[i * OFFSET + 4] = 0;
-      this.viewer[i * OFFSET + 5] = 0;
-      this.viewer[i * OFFSET + 6] = 0;
+    for (let i = 0; i < this._numCells; i++) {
+      this._viewer[i * OFFSET] = x;
+      this._viewer[i * OFFSET + 1] = y;
+      x == this._width - 1 ? (x = 0, y++) : x++;
+      this._viewer[i * OFFSET + 2] = 0;
+      this._viewer[i * OFFSET + 3] = 0;
+      this._viewer[i * OFFSET + 4] = 0;
+      this._viewer[i * OFFSET + 5] = 0;
+      this._viewer[i * OFFSET + 6] = 0;
     }
   }
   public setCell(GridCell: GridCell) {
-    const index = (GridCell.y * this.width + GridCell.x) * OFFSET;
+    const index = (GridCell.y * this._width + GridCell.x) * OFFSET;
     if (
-      this.viewer[index] != GridCell.x || this.viewer[index + 1] != GridCell.y
+      this._viewer[index] != GridCell.x || this._viewer[index + 1] != GridCell.y
     ) {
       throw new Error(
         `Buffer isn't lined up: input ${GridCell.x},${GridCell.y}, buffer ${
-          this.viewer[index]
-        },${this.viewer[index + 1]}`,
+          this._viewer[index]
+        },${this._viewer[index + 1]}`,
       );
     }
     if (GridCell.plantID >= 0) {
-      this.viewer[index + 2] = GridCell.plantID;
+      this._viewer[index + 2] = GridCell.plantID;
     }
     if (GridCell.growthLevel >= 0) {
-      this.viewer[index + 3] = GridCell.growthLevel;
+      this._viewer[index + 3] = GridCell.growthLevel;
     }
     if (GridCell.sun >= 0) {
-      this.viewer[index + 4] = GridCell.sun;
+      this._viewer[index + 4] = GridCell.sun;
     }
     if (GridCell.water >= 0) {
-      this.viewer[index + 5] = GridCell.water;
+      this._viewer[index + 5] = GridCell.water;
     }
     if (GridCell.backgroundID >= 0) {
-      this.viewer[index + 6] = GridCell.backgroundID;
+      this._viewer[index + 6] = GridCell.backgroundID;
     }
   }
 
   public getCell(x: number, y: number): GridCell {
-    const index = (y * this.width + x) * OFFSET;
-    if (this.viewer[index] != x || this.viewer[index + 1] != y) {
+    const index = (y * this._width + x) * OFFSET;
+    if (this._viewer[index] != x || this._viewer[index + 1] != y) {
       throw new Error(
-        `Buffer isn't lined up: input ${x},${y}, buffer ${this.viewer[index]},${
-          this.viewer[index + 1]
+        `Buffer isn't lined up: input ${x},${y}, buffer ${this._viewer[index]},${
+          this._viewer[index + 1]
         }`,
       );
     }
     return {
-      x: this.viewer[index],
-      y: this.viewer[index + 1],
-      plantID: this.viewer[index + 2],
-      growthLevel: this.viewer[index + 3],
-      sun: this.viewer[index + 4],
-      water: this.viewer[index + 5],
-      backgroundID: this.viewer[index + 6],
+      x: this._viewer[index],
+      y: this._viewer[index + 1],
+      plantID: this._viewer[index + 2],
+      growthLevel: this._viewer[index + 3],
+      sun: this._viewer[index + 4],
+      water: this._viewer[index + 5],
+      backgroundID: this._viewer[index + 6],
     };
+  }
+
+  public gridChange (viewer: Uint8Array){
+    this.stateArray.push(viewer);
+  }
+
+  public saveToJSON(){
+    return JSON.stringify({
+      numCells: this._numCells,
+      buffer: this._buffer,
+      viewer: this._viewer,
+      stateArray: this.stateArray,
+      width: this._width
+    })
   }
 }
