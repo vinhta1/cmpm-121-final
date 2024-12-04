6c5382676421c6958d54db810e617775bc714814
lmurayam <130516683+lmurayam@users.noreply.github.com>
2024-12-04 02:59:36 +0000

    Testing Vite PWA plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
+import {VitePWA} from "vite-plugin-pwa"
+
 export default {
   base: Deno.env.get("REPO_NAME") || "/project",
   server: {
     hmr: false,
   },
+  plugins:[VitePWA({ registerType: 'autoUpdate' })],
 };
