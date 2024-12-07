5adad1b0bba72c5fe25a08e9ef77fc88ff019725
Tony Pau <topau@ucsc.edu>
2024-12-06 22:04:23 -0800

    Custom Weather goes to turn 49

--- src/scenario-yaml.ts
+++ src/scenario-yaml.ts
@@ -6,16 +6,33 @@ weatherEffects:
     waterChange: -1
   rainy:
     sunChange: 1
-    waterChange: 5
+    waterChange: 7
   clear:
-    sunChange: 2
-    waterChange: 0
+    sunChange: 3
+    waterChange: 3
 
 weatherCycle:
   5: sunny
   7: rainy
+  10: sunny
+  12: clear
+  14: rainy
+  15: sunny
+  20: sunny
+  21: rainy
+  24: clear
+  25: sunny
+  28: rainy
+  30: sunny
+  35: rainy
+  36: clear
+  40: sunny
+  42: rainy
+  45: sunny
+  48: clear
+  49: rainy
 
 winCondition:
-  totalHarvests: 1
+  totalHarvests: 12
   description: "Harvest 12 plants in total to win the game."
 `
